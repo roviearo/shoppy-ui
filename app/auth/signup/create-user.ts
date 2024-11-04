acd16688fb78eb1816a4aeb43a0f4e69cbb24817
Akhmad Roviansyah <roviearo@gmail.com>
2024-11-04 16:18:59 +0700

    signup

---
+++ app/auth/signup/create-user.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import API_URL from "@/app/constants/api";
+import { getErrorMessage } from "@/app/util/errors";
+import { redirect } from "next/navigation";
+
+export default async function createUser(_prevState: any, formData: FormData) {
+  const res = await fetch(`${API_URL}/users`, {
+    method: "POST",
+    body: formData,
+  });
+  const parsedRes = await res.json();
+  if (!res.ok) {
+    console.log(parsedRes);
+    return { error: getErrorMessage(parsedRes) };
+  }
+  redirect("/");
+}
